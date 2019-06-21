39d89c27067aab66ff355474afb9d9e298de151a
glgraca <30938064+glgraca@users.noreply.github.com>
2019-06-21 13:45:42 -0300

    Create apex_advisor.sql

---
+++ apex_advisor.sql
@@ -0,0 +1,13 @@
+create or replace package apex_advisor AS 
+
+  procedure create_apex_session(
+    p_app_id in apex_applications.application_id%type,
+    p_app_user in apex_workspace_activity_log.apex_user%type,
+    p_app_page_id in apex_application_pages.page_id%type default 1
+  );
+
+  procedure execute_advisor(p_app_id in number, p_log_date date default sysdate);
+  
+  procedure execute_advisor;
+
+end apex_advisor;
