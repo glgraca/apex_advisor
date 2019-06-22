ebf860b4033de7f89ae76c13947fa240c370f6e5
glgraca <30938064+glgraca@users.noreply.github.com>
2019-06-22 05:28:00 -0300

    Update apex_advisor.sql
    Added execute_advisor_async()

--- apex_advisor.sql
+++ apex_advisor.sql
@@ -5,6 +5,8 @@ create or replace package apex_advisor AS
     p_app_user in apex_workspace_activity_log.apex_user%type,
     p_app_page_id in apex_application_pages.page_id%type default 1
   );
+  
+  procedure execute_advisor_async(p_app_id in number, p_log_date date default sysdate);
 
   procedure execute_advisor(p_app_id in number, p_log_date date default sysdate);
   
